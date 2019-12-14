724cce12c97a3bc8945d6e275fea32f7001d2f47
Maciej Szklarczyk <maciej.szklarczyk@protonmail.com>
2019-12-14 18:17:25 +0100

    Create request handling

--- public_html/OmdbService.php
+++ public_html/OmdbService.php
@@ -1,35 +1,37 @@
 <?php
 
-searchByTitle('daredevil', ['type' => 'movie']);
-
-function findByTitle(string $title)
-{
-    prettyPrintResult(sendRequest('t', urlencode($title)));
-}
-
-function searchByTitle(string $title, $parameters = [])
-{
-    prettyPrintResult(sendRequest('s', urlencode($title), $parameters));
-}
-
-function sendRequest(string $queryField, string $queryValue, array $parameters = [])
-{
-    $ch = curl_init();
-    $request = "http://www.omdbapi.com/?apikey=70bc1bb9&";
-    $request .= $queryField . "=" . $queryValue;
-    if (!empty($parameters)) {
-        $request .= "&" . http_build_query($parameters);
+class OmdbService {
+    public function findByTitle(string $title)
+    {
+        return $this->sendRequest('t', urlencode($title));
     }
-    curl_setopt($ch, CURLOPT_URL, $request);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    return $output;
+
+    public function searchByTitle(string $title, $parameters = [])
+    {
+        return $this->sendRequest('s', urlencode($title), $parameters);
+    }
+
+    protected function sendRequest(string $queryField, string $queryValue, array $parameters = [])
+    {
+        $ch = curl_init();
+        $request = "http://www.omdbapi.com/?apikey=70bc1bb9&";
+        $request .= $queryField . "=" . $queryValue;
+        if (!empty($parameters)) {
+            $request .= "&" . http_build_query($parameters);
+        }
+        curl_setopt($ch, CURLOPT_URL, $request);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+
+    public function prettyPrintResult($requestResult)
+    {
+        echo "<pre>";
+        var_dump(json_decode($requestResult));
+        echo "</pre>";
+    }
 }
 
-function prettyPrintResult($requestResult)
-{
-    echo "<pre>";
-    var_dump(json_decode($requestResult));
-    echo "</pre>";
-}
+
