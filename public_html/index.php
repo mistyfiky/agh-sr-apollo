9b43e9ecc062006981b8c966d7efd812c8ec778d
maciejszklarczyk <34677047+maciejszklarczyk@users.noreply.github.com>
2019-12-14 18:18:45 +0100

    Create endpoint search (#2)
    * Add basic connection to test api search and find by title
    
    * Add volume mount command to readme
    
    * Rename php file to OmdbService
    
    * Create request handling

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,42 @@
 <?php
+include 'OmdbService.php';
 
-die(json_encode([
-    'meta' => [
-        'message' => 'turbo źródler 3000!'
-    ]
-]));
+if (!isset($_GET['function'])) {
+    die(json_encode([
+        'meta' => [
+            'message' => 'turbo źródler 3000!'
+        ]
+    ]));
+} elseif ($_GET['function']) {
+    $omdbService = new OmdbService();
+    $functionName = $_GET['function'];
+    switch ($functionName) {
+        case 'searchByTitle':
+            $params = [];
+            $_POST['parameters'] ?: $params['type'] = $_POST['parameters'];
+            $result = json_decode($omdbService->searchByTitle($_POST['title'], $params), true);
+            if ($result["Response"] !== "True") {
+                die("Error");
+            }
+            $cards = "";
+            $mod4 = 0;
+            foreach ($result["Search"] as $movie) {
+                if ($movie["Poster"] == 'N/A') {
+                    continue;
+                }
+                if (($mod4 % 4) == 0) {
+                    $cards .= '<div class="row">';
+                }
+                $cards .= '<div class="col s12 m3" ><div class="card"><div class="card-image">';
+                $cards .= '<img src="' . $movie["Poster"] . '">';
+                $cards .= '<span class="card-title">' . $movie["Title"] . '</span></div><div class="card-content"><p>IMDB ID: ';
+                $cards .= $movie["imdbID"] . '</p></div></div></div>';
+                if (($mod4 % 4) == 3) {
+                    $cards .= '</div>';
+                }
+                $mod4++;
+            }
+            die($cards);
+    }
+
+}
