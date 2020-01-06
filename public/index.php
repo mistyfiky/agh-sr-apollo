44a5a112b9149e90ae71a1884ba904d6c7453d77
Maciej Szklarczyk <maciej.szklarczyk@protonmail.com>
2020-01-06 14:49:25 +0100

    Add available genres select, add searchByGenre

--- public/index.php
+++ public/index.php
@@ -36,6 +36,31 @@ if (!isset($_GET['function'])) {
                 $mod4++;
             }
             die($cards);
+        case 'searchByGenre':
+            $movies = $omdbService->searchByGenre($_POST['genre']);
+            $cards = "";
+            $mod4 = 0;
+            foreach ($movies as $movie) {
+                if (($mod4 % 4) == 0) {
+                    $cards .= '<div class="row flex">';
+                }
+                $cards .= '<div class="col s12 m3" ><div class="card"><div class="card-image">';
+                $cards .= '<img src="' . $movie['poster_image'] . '">';
+                $cards .= '<span class="card-title">' . $movie['title'] . '</span></div><div class="card-content"><p>ID: ';
+                $cards .= $movie['id'] . '</p></div></div></div>';
+                if (($mod4 % 4) == 3) {
+                    $cards .= '</div>';
+                }
+                $mod4++;
+            }
+            die($cards);
+        case 'getAvailableGenres':
+            $genres = $omdbService->availableGenres();
+            $options = "";
+            foreach ($genres as $genre) {
+                $options .= '<option value="' . $genre['id'] . '">' . $genre['name'] . '</option>';
+            }
+            die($options);
     }
 
 }
